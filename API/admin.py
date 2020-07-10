abf56c59e0e776b1689c175a34494d6b464bff22
iamdanialkamali <daniel.kamali@yahoo.com>
2020-07-10 11:59:06 +0430

    admin panel fix

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User,Business,Category,Service,TimeTable,Reserve,Sans,Transaction
+from .models import  Business, Category, Service, TimeTable, Reserve, Sans, Transaction, Wallet, Review, \
+    BusinessFile, ServiceFile, MessageFile
 
 admin.site.register(Business)
 admin.site.register(Category)
@@ -7,6 +8,12 @@ admin.site.register(Service)
 admin.site.register(TimeTable)
 admin.site.register(Reserve)
 admin.site.register(Sans)
+admin.site.register(User)
+admin.site.register(Wallet)
+admin.site.register(Review)
 admin.site.register(Transaction)
+admin.site.register(BusinessFile)
+admin.site.register(ServiceFile)
+admin.site.register(MessageFile)
 
 # Register your models here.
