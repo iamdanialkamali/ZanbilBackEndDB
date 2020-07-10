f63ea3524aaedad6ca0a6e84ca23ad1b79c0b596
iamdanialkamali <daniel.kamali@yahoo.com>
2020-07-11 00:12:33 +0430

    fix middleware 2

--- API/middleware.py
+++ API/middleware.py
@@ -92,8 +92,8 @@ def simple_middleware(get_response):
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        response = get_response(request)
         data = request.body.decode()
+        response = get_response(request)
         try:
             if len(response.content)<4000:
                 orm.safeInsert(
